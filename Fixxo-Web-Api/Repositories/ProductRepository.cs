d92be7b63b1bd1cec5d741f271ff575042c1d9a4
mattekiwi <kivimatte@gmail.com>
2023-04-12 14:09:40 +0200

    update

---
+++ Fixxo-Web-Api/Repositories/ProductRepository.cs
@@ -0,0 +1,44 @@
+namespace Fixxo_Web_Api.Repositories;
+
+public class ProductRepository
+{
+    /* private readonly DataContext _context;
+     public ProductRepository(DataContext context)
+     {
+         _context = context;
+     }
+    */
+    
+    /*
+     public async Task<IEnumerable<Product>> GetAllAsync()
+     {
+         return await _context.Products.ToListAsync();
+     }
+     */
+    /*
+     public async Task<Product> GetByArticleNumberAsync(string articleNumber)
+     {
+         return await _context.Products.FirstOrDefaultAsync(p => p.ArticleNumber == articleNumber);
+     }
+     */
+    /*
+     public async Task<IEnumerable<Product>> GetByTagAsync(string tagName)
+     {
+         return await _context.Products.Where(p => p.Tags.Any(t => t.Name == tagName)).ToListAsync();
+     }
+     */
+    /*
+        public async Task<IEnumerable<Product>> GetByCategoryAsync(string categoryName)
+        {
+            return await _context.Products.Where(p => p.Category.Name == categoryName).ToListAsync();
+        }
+        */
+    /*
+     * public async Task<Product> CreateAsync(ProductHttpRequest product)
+     * {
+     *  _context.Products.Add(product);
+     * await _context.SaveChangesAsync();
+     * return product;
+     * }
+     */
+}
